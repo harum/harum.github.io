c086b910a0339992cb6427d6e7cb60749c0966d0
Putu Harum Bawa <lightharum.108@gmail.com>
2018-02-11 01:26:59 +0700

    add typography.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,10 @@ export default () =>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     <br />
     <div>
-      <Link to="/page-2/">Link</Link>
+      <Link to="/page-2/">Page 2</Link>
+    </div>
+    <div>
+      <Link to="/page-3/">Page 3</Link>
     </div>
     <div>
       <Link to="/counter/">Counter</Link>
