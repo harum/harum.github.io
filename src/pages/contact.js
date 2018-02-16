ef2c985ab2cbdb2184d97b5882da0fd9a9d4e9e8
Putu Harum Bawa <lightharum.108@gmail.com>
2018-02-16 16:52:48 +0700

    add layouts

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:lightharum.108@gmail.com">lightharum.108@gmail.com</a>
+    </p>
+  </div>
+);
