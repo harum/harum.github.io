5ee12261f8b04aa28b630bfa30f4261d097084f9
Putu Harum Bawa <lightharum.108@gmail.com>
2018-04-22 23:30:03 +0700

    add photo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,21 @@
 import React from "react";
+import g from "glamorous";
+import { rhythm } from "../utils/typography";
+import avatar from "../images/harum_avatar.jpg";
 
 export default () => (
   <div>
-    <p>Hello. My name is Putu Harum Bawa. I am from Bali, but currently working in
-    Jakarta, Indonesia. I currently work at <a href="https://www.bukalapak.com/">Bukalapak</a> as Front End Developer since August 2015.</p>
+    <p>
+      <g.Img
+        height='130px'
+        src={avatar}
+        alt='Putu Harum Bawa'
+        margin={rhythm(0.25)}
+        float='left'
+      />
+      Hello. My name is Putu Harum Bawa. I am from Bali, but currently working in
+      Jakarta, Indonesia. I currently work at <a href="https://www.bukalapak.com/">Bukalapak</a> as Front End Developer since August 2015.
+    </p>
 
     <p>I believe that the best way to learn is to teach other. In this blog I try to
     explain and rewrite what I have learned. I really apreciated feedback and discussion from you.</p>
