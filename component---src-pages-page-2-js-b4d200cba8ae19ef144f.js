e4298605255281a44d62a96ac5a60f857c173844
Putu Harum Bawa <lightharum.108@gmail.com>
2018-07-14 16:12:45 +0700

    Updates

---
+++ component---src-pages-page-2-js-b4d200cba8ae19ef144f.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xc6c285f8fd10],{178:function(e,t,l){"use strict";function u(e){return e&&e.__esModule?e:{default:e}}t.__esModule=!0;var a=l(6),n=u(a),d=l(84),f=u(d);t.default=function(){return n.default.createElement("div",null,n.default.createElement("p",null,"Hellow world from my second Gatsby page"),n.default.createElement(f.default,{to:"/"},"back home"))},e.exports=t.default}});
+//# sourceMappingURL=component---src-pages-page-2-js-b4d200cba8ae19ef144f.js.map
--- src/components/navigation.js
+++
@@ -1,5 +0,0 @@
-export default ({ children }) => (
-  <nav>
-    { children }
-  </nav>
-)
